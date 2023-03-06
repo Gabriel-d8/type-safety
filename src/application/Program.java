3ea8bec114262f0b6eb321ec00899706e0d9efdb
Gabriel D'Andréa <webgabrielaraujo@gmail.com>
2023-03-06 12:37:35 -0300

    Criando tipo Genérico delimitado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,31 +1,41 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
-import java.util.Scanner;
 
-import services.PrintService;
+import entities.Product;
+import services.CalculationService;
 
 public class Program {
 
 	public static void main(String[] args) {
 	
-		Scanner sc = new Scanner(System.in);
 		Locale.setDefault(Locale.US);
 		
-		PrintService<String> ps = new PrintService<>();
+		List<Product> list = new ArrayList<>();
 		
-		System.out.print("How many values? ");
-		int n = sc.nextInt();
+		String path = "C:\\temp\\in.txt";
 		
-		for (int i = 1; i <= n; i++) {
-			ps.addValue(sc.next());
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
+			
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(",");
+				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+				line = br.readLine();
+			}
+			
+			Product x = CalculationService.max(list);
+			System.out.println("Most Expensive:");
+			System.out.println(x);
+			
+		} catch (IOException e) {
+			System.out.println("Error!" + e.getMessage());
 		}
-		
-		ps.print();
-		System.out.println();
-		System.out.println("First: " + ps.firstElement());
-		
-		sc.close();
 		
 	}
 
